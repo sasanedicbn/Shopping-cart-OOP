e5da2ec8e1b76b60a8c0666bbfe65056299ca1c3
Sasa Nedic <svetasmak0@gmail.com>
2024-01-15 14:23:45 +0100

    Create displayCars

--- shopping-cart-OOP/script.js
+++ shopping-cart-OOP/script.js
@@ -8,4 +8,37 @@ class LogicCars {
     this.cars = [...data];
     this.filteredCars = [...this.cars];
   }
+
+  displayCars(elementCars) {
+    car.innerHTML = "";
+    console.log(elementCars);
+    elementCars.forEach((data) => {
+      const cars = document.createElement("div");
+
+      const backgroundColor =
+        data.available === "yes" ? "rgb(91, 199, 91)" : "rgb(214, 81, 81)";
+      const availabilityText = data.available === "yes" ? "Yes" : "No";
+
+      cars.innerHTML = `<div class="cart-row"> 
+                <div class="first-data">
+                    <h1>${data.name}</h1>
+                    <img src="${data.image}" alt="${data.image}"/>
+                </div>
+                <div class="second-data">
+                    <p><strong>Brand:</strong> ${data.brand}</p>
+                    <p><strong>Manufactured Year:</strong> ${data.manufacturedYear}</p>
+                    <p><strong>Doors:</strong> ${data.doors}</p>
+                    <p><strong>Price:</strong><span class="price">$${data.price}</span></p>
+                </div>
+                <p style="background-color: ${backgroundColor};"><strong>Available:</strong> ${availabilityText}</p>
+                <button class="deleteCar" data-carId=${data.id}>Delete</button>
+            </div>`;
+
+      const deleteButton = cars.querySelector(".deleteCar");
+      deleteButton.addEventListener("click", () => {
+        this.deleteCar(data.id);
+      });
+      car.appendChild(cars);
+    });
+  }
 }
