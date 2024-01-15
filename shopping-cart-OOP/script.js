bf92802257b0e3b7a797a458b9c5de6a34ec7de8
Sasa Nedic <svetasmak0@gmail.com>
2024-01-15 14:22:53 +0100

    Create constructor

--- shopping-cart-OOP/script.js
+++ shopping-cart-OOP/script.js
@@ -2,3 +2,10 @@ import { cars as data } from "./data";
 const car = document.querySelector(".car");
 const sortOptions = document.querySelector(".sortOptions");
 const availability = document.querySelector(".availability");
+
+class LogicCars {
+  constructor(data) {
+    this.cars = [...data];
+    this.filteredCars = [...this.cars];
+  }
+}
