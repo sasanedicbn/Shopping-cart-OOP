a94a1df4ee7c871befb88e4d449b744bd89b345b
Sasa Nedic <svetasmak0@gmail.com>
2024-01-15 09:43:09 +0100

    Select all variables for project

---
+++ shopping-cart-OOP/script.js
@@ -0,0 +1,4 @@
+import { cars as data } from "./data";
+const car = document.querySelector(".car");
+const sortOptions = document.querySelector(".sortOptions");
+const availability = document.querySelector(".availability");
